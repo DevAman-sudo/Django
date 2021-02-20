3400e59be30fdaecadebf05254ad32935c4a5069
DevAman-sudo <devaman.net@gmail.com>
2021-02-20 17:40:42 +0545

    s

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact")
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# urls dispatching
 def index(request) :
     return HttpResponse("this is devaman")
+    
+def about(request) :
+    return HttpResponse("this is about page")
+    
+def contact(request) :
+    return HttpResponse("this is contact page")
+    
