8e211419ba11dd9cc90faeaf6c0f414cbddd777f
DevAman-sudo <devaman.net@gmail.com>
2021-02-25 06:11:43 +0545

    saved form data to database

--- home/views.py
+++ home/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 # urls dispatching
 def index(request) :
     context = {
-        'message': 'Hello World!'
-    }
+    'message': 'Hello World!'
+}
     return render(request , 'index.html', context)
-    
+
 def about(request) :
     return render(request , 'about.html')
-    
+
 def contact(request) :
+    if request.method == "POST" :
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        contact = Contact(name = name , email = email)
+        contact.save()
+        
     return render(request , 'contact.html')
